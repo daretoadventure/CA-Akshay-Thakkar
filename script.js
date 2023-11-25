949a2c1a1d64eb5cbefeea07bdf34b185a0c8884
daretoadventure <152001187+daretoadventure@users.noreply.github.com>
2023-11-25 15:51:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const navBtn = document.getElementById("navBtn");
+const navLinks = document.getElementById("navLinksSm");
+const navMenu = document.getElementById("navMenu");
+navBtn.addEventListener("click", () => {
+	console.log("clicked");
+	navLinks.classList.toggle("active");
+	if (navMenu.classList.contains("fa-bars")) {
+		navMenu.classList.remove("fa-bars");
+		navMenu.classList.add("fa-xmark");
+	} else {
+		navMenu.classList.add("fa-bars");
+		navMenu.classList.remove("fa-xmark");
+	}
+});
+
+const form = document.querySelector(".form");
+const formData = new FormData(form);
+const errorMsg = document.querySelector("#errorMsg");
+const submitBtn = document.querySelector("#submit");
+const validateField = document.querySelector("#validate");
+const result = document.querySelector(".result");
+const loader = document.querySelector(".loader");
+
+const formSubmitUrl =
+	"https://script.google.com/macros/s/AKfycbzWC2d8AeRWsCf48AiQNJzmpln2iaXiVCrI_zPr57RT6BB5TuTwWBw9DLaJ26QYVl8-oA/exec";
+
+function validateForm(form) {
+	const formData = new FormData(form);
+	const nameField = formData.get("Name");
+	const phone = formData.get("Phone");
+	const email = formData.get("Email");
+	const qualification = formData.get("Course");
+	console.log(qualification);
+	let isValid = true;
+	if (!nameField) {
+		isValid = false;
+		validateField.classList.add("validationError");
+	} else if (!email) {
+		isValid = false;
+		validateField.classList.add("validationError");
+	} else if (!phone) {
+		isValid = false;
+		validateField.classList.add("validationError");
+	} else if (!qualification) {
+		isValid = false;
+		validateField.classList.add("validationError");
+	} else {
+		validateField.classList.remove("validationError");
+	}
+	return isValid;
+}
+form.addEventListener("submit", function (event) {
+	event.preventDefault(); // Prevent the default form submission behavior
+	let isValid = validateForm(form); // Validate form fields
+	if (!isValid) {
+		validateField.innerHTML = "All fields are required";
+		return false;
+	} else {
+		validateField.innerHTML = "";
+		console.log(...new FormData(form));
+		loader.style.display = "block";
+
+		fetch(formSubmitUrl, {
+			method: "POST",
+			body: new FormData(form),
+		})
+			.then((response) => {
+				loader.style.display = "none";
+				return response.json();
+			})
+			.then((data) => {
+				loader.style.display = "none";
+				// Display a success message
+				result.innerHTML = "Thanks for your interest. We'll get in touch with you soon!";
+				form.reset();
+			})
+			.catch((error) => {
+				loader.style.display = "none";
+				console.log(error);
+			});
+	}
+});
